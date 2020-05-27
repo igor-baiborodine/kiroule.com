b2e45817465dd4c3ed35b34b9cd534c413e52b10
Igor Baiborodine <igor.baiborodine@yandex.com>
2020-05-27 09:34:18 -0400

    Update algolia-helper.sh

--- netlify/algolia-helper.sh
+++ netlify/algolia-helper.sh
@@ -1,4 +1,4 @@
 #!/usr/bin/env bash
 
-sed -i "s/ALGOLIA_APP_ID_PLACEHOLDER/${HUGO_PARAM_ALGOLIA_APP_ID}/g" ./config.toml
-sed -i "s/ALGOLIA_API_KEY_PLACEHOLDER/${HUGO_PARAM_ALGOLIA_API_KEY}/g" ./config.toml
+sed -i "s/ALGOLIA_APP_ID_PLACEHOLDER/${HUGO_PARAM_ALGOLIA_APP_ID}/g" ../config.toml
+sed -i "s/ALGOLIA_API_KEY_PLACEHOLDER/${HUGO_PARAM_ALGOLIA_API_KEY}/g" ../config.toml
