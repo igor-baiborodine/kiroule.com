8d57823d2dc7fcf1826459eec29fa26566f90cfd
Igor Baiborodine <igor.baiborodine@yandex.com>
2020-05-27 09:45:34 -0400

    Update algolia-helper.sh

--- netlify/algolia-helper.sh
+++ netlify/algolia-helper.sh
@@ -1,5 +1,5 @@
 #!/usr/bin/env bash
-echo "Igor here: "
+echo "Igor here: ${HUGO_PARAM_ALGOLIA_APP_ID}"
 ls -al
 sed -i "s/ALGOLIA_APP_ID_PLACEHOLDER/${HUGO_PARAM_ALGOLIA_APP_ID}/g" config.toml
 sed -i "s/ALGOLIA_API_KEY_PLACEHOLDER/${HUGO_PARAM_ALGOLIA_API_KEY}/g" config.toml
