b79c80965b32bb735f9e97d9797481f95e892101
Igor Baiborodine <igor.baiborodine@yandex.com>
2020-05-27 12:32:31 -0400

    Config (#64)
    Update config.toml

---
+++ netlify/algolia-helper.sh
@@ -0,0 +1,5 @@
+#!/usr/bin/env bash
+
+# test to not expose sensitive stuff in config.toml
+#sed -i "s/ALGOLIA_APP_ID_PLACEHOLDER/${HUGO_PARAM_ALGOLIA_APP_ID}/g" config.toml
+#sed -i "s/ALGOLIA_API_KEY_PLACEHOLDER/${HUGO_PARAM_ALGOLIA_API_KEY}/g" config.toml
