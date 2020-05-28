38c25382925d54b84abae8276c32eed148874f12
Igor Baiborodine <igor.baiborodine@yandex.com>
2020-05-28 18:19:39 -0400

    Update algolia-helper.sh

--- netlify/algolia-helper.sh
+++ netlify/algolia-helper.sh
@@ -4,4 +4,5 @@
 #sed -i "s/ALGOLIA_APP_ID_PLACEHOLDER/${HUGO_PARAM_ALGOLIA_APP_ID}/g" config.toml
 #sed -i "s/ALGOLIA_API_KEY_PLACEHOLDER/${HUGO_PARAM_ALGOLIA_API_KEY}/g" config.toml
 
+python3 --version
 pip install --upgrade 'algoliasearch>=2.0,<3.0'
