3a4623d0fcffecd41886babf26703c46d8a4cabf
Igor Baiborodine <igor.baiborodine@yandex.com>
2020-06-02 07:25:38 -0400

    Update run-index-upload.sh

--- algolia/run-index-upload.sh
+++ algolia/run-index-upload.sh
@@ -4,7 +4,7 @@ while getopts "pf:a:k:n:" opt; do
   case $opt in
   # netlify
   p)
-    pip3 install --upgrade 'algoliasearch>=2.0,<3.0'
+    python3 -m pip install --upgrade 'algoliasearch>=2.0,<3.0'
     # Environment variables below (except PWD) should be configured
     # in the section 'Build & deploy/Environment variables' of your site in Netlify;
     # Alternatively, the ALGOLIA_INDEX_NAME variable can be defined in the netlify.toml file.
