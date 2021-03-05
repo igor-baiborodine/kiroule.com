5438d7954883a12f8a2a05e601b00cd722736020
Igor Baiborodine <igor.baiborodine@yandex.com>
2021-03-05 08:20:13 -0500

    Update data-upload.js

--- algolia/data-upload.js
+++ algolia/data-upload.js
@@ -1,1 +1,15 @@
-console.log("Igor here!")
+var argv = require("yargs/yargs")(process.argv.slice(2))
+  .boolean("c").alias("c", "clear-index").describe("c", "Clear Algolia index before upload")
+  .alias("f", "index-file").nargs("f", 1).describe("f", "Index file to upload to Algolia")
+  .alias("a", "app-id").nargs("a", 1).describe("a", "Algolia application ID")
+  .alias("k", "admin-api-key").nargs("k", 1).describe("k", "Algolia admin API key")
+  .alias("n", "index-name").nargs("n", 1).describe("n", "Algolia index name")
+  .alias("u", "base-url").nargs("u", 1).describe("u", "Site base URL")
+  .demandOption(["f", "a", "k", "n"])
+  .help("h")
+  .alias("h", "help")
+  .argv;
+
+if (argv["clear-index"]) {
+  console.log("Clearing index...");
+}
