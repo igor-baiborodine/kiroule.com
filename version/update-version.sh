d94c03c29c096841f1cfec61e3dd4db4cf221e99
Igor Baiborodine <igor@kiroule.com>
2023-02-01 13:40:57 -0500

    Update theme to v3.19.0

--- version/update-version.sh
+++ version/update-version.sh
@@ -2,3 +2,4 @@
 
 version=$(grep -o 'v[0-9]\.[0-9][0-9]\.[0-9]' go.mod)
 sed -i "s/creditsText = .*$/creditsText = \"Bilberry Hugo Theme $version\"/" config/_default/config.toml
+sed -i "s/creditsUrl = .*$/creditsUrl = \"https://github.com/Lednerb/bilberry-hugo-theme/tree/$version\"/" config/_default/config.toml
