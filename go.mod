280ed419eb360e95ee4aac6dfe561b48c418a59a
Igor Baiborodine <igor@kiroule.com>
2023-06-16 16:48:05 -0400

    feat: upgrade to v4.0.0-alpha01 (#292)
    * migrate pages to v4
    
    * update Hugo version in netlify.toml
    
    * upgrade theme version to v4
    
    * upgrade version extraction from go.mod

--- go.mod
+++ go.mod
@@ -2,4 +2,4 @@ module github.com/igor-baiborodine/kiroule.com
 
 go 1.18
 
-require github.com/Lednerb/bilberry-hugo-theme/v3 v3.24.1 // indirect
+require github.com/Lednerb/bilberry-hugo-theme/v4 v4.0.0-alpha01 // indirect
--- version/update-version.sh
+++ version/update-version.sh
@@ -1,5 +1,5 @@
 #!/usr/bin/env bash
 
-version=$(grep -o 'v[0-9]\.[0-9][0-9]\.[0-9]' go.mod)
+version=$(grep -Po '(?<=v4\s)[^\s//]+' go.mod)
 sed -i "s/creditsText = .*$/creditsText = \"Bilberry Hugo Theme $version\"/" config/_default/config.toml
 sed -i "s/creditsUrl = .*$/creditsUrl = \"https:\/\/github.com\/Lednerb\/bilberry-hugo-theme\/tree\/$version\"/" config/_default/config.toml
